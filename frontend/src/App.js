fa04bcbdada78a20b1ae3b85cad772e28d25ebdc
filippociandy10 <61950298+filippociandy10@users.noreply.github.com>
2022-05-24 14:48:13 -0600

    Fil/theme color (#1)
    * Create color theme MUI
    
    * Removed comments and change base to develop branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import { createTheme } from '@mui/material/styles';
 import { Header, Footer } from './components/shared'
+
+
+/* eslint-disable no-unused-vars */
+var theme = createTheme({
+    palette: {
+        primary: {
+            light: '#FFFFFF', // White
+            main: '#147BE5', // Primary color
+            dark: '#0B2538', // Dark blue
+            contrastText: '#000',
+        },
+        secondary: {
+            light: '#F8F8F8', //Light grey
+            main: '#AFAFAF', // Grey
+            dark: '#333333', //Dark grey
+            contrastText: '#000',
+        },
+    },
+});
+/* eslint-enable no-unused-vars */
+
 
 function App() {
     return (
