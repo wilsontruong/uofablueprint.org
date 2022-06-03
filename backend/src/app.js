9e825a281e49dad4fa01687279469f4b755960ad
Dimas Sudjito <almuhtad@ualberta.ca>
2022-06-02 20:20:14 -0600

    setup a skeleton app and connect it to mongodb atlas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+const dotenv = require('dotenv')
 
+const membersRouter = require('./routes/members')
+
+// general setup
+const PORT = 6060
+const app = express()
+dotenv.config()
+
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_ADDR, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+// handle routing
+app.use(bodyParser.json())
+app.use('/members', membersRouter)
+
+app.listen(PORT, () => {
+    console.log(`uofablueprint.org-backend listening on port ${PORT}`)
+})
