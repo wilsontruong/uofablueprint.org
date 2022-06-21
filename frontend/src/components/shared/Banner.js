43c74013c18b8cd1685b4b04cc24059d779868e9
GurveerSohal <74379063+GurveerSohal@users.noreply.github.com>
2022-06-20 20:34:39 -0600

    Issue24 banner component (#11)
    * styled generic banner component
    
    * using custom theme for colors

--- frontend/src/components/shared/Banner.js
+++ frontend/src/components/shared/Banner.js
@@ -1,7 +1,56 @@
+import { Button, Typography } from '@mui/material'
+import PropTypes from 'prop-types'
+import { Box } from '@mui/system'
 import React from 'react'
+import { Theme } from '../../config/Theme'
 
-function Banner() {
-    return <div>-generic banner component placeholder-</div>
+
+function Banner({header, body, buttonText, buttonLink}) {
+    return (
+        <Box sx={{
+            background: Theme.palette.primary.dark,
+            color: Theme.palette.primary.light
+        }}>
+            <Typography variant='h4' sx={{
+                p: 2,
+                pl: 4,
+                fontWeight: 'medium'
+            }}>
+                {header}
+            </Typography>
+
+            <Typography variant='body1' sx={{
+                p: 2,
+                pl: 4,
+                fontSize: 20,
+                fontWeight: 'light'
+            }}>
+                {body}
+            </Typography>
+
+            <Button variant='outlined' size='small' href={buttonLink} target='_blank' rel='noopener noreferrer' sx={{
+                m: 2,
+                ml: 4,
+                color: Theme.palette.primary.light,
+                borderColor: Theme.palette.secondary.light,
+                "&:hover": {
+                    borderColor: Theme.palette.primary.light
+                },
+            }}>
+                {buttonText}
+            </Button>
+
+        </Box>
+    )
 }
+
+
+Banner.propTypes = {
+    header: PropTypes.string,
+    body: PropTypes.string,
+    buttonText: PropTypes.string,
+    buttonLink: PropTypes.string
+}
+
 
 export default Banner
