ef7375dceec72f36c366aa6c782e7073b4f237ea
GurveerSohal <74379063+GurveerSohal@users.noreply.github.com>
2022-06-26 16:12:40 -0600

    Issue23 mission statement (#10)
    * styled mission statement component
    
    * using custom theme for color
    
    * test for mission statement component
    
    * made requested changes
    
    * change text body color and heading size

--- frontend/src/components/about/Mission.js
+++ frontend/src/components/about/Mission.js
@@ -1,7 +1,32 @@
+import { Typography } from '@mui/material'
 import React from 'react'
+import { Box } from '@mui/system';
+import { Theme } from '../../config/Theme';
 
 function Mission() {
-    return <div>-mission component placeholder-</div>
+    return (
+        <Box sx={{
+            background: Theme.palette.secondary.light,
+            textAlign: 'center',
+        }}>
+            <Typography variant='h4' gutterBottom={true} color={Theme.palette.primary.main} sx={{
+                pt: 4,
+                fontWeight: 'bold'
+            }}>
+                Our Mission Statement
+            </Typography>
+
+            <Typography variant='body1' color={Theme.palette.secondary.dark} sx={{
+                p: 4,
+                fontSize: 22,
+                width: 700,
+                mx: 'auto'
+            }}>
+                To make beautiful engineering accessible and useful for those who create
+                communities and promote public welfare.
+            </Typography>
+        </Box>
+    )
 }
 
 export default Mission
