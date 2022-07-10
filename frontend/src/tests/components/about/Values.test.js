15a96a0b27d7c9e715da2c28c61d784dda4ea7e8
Dimas A. Sudjito <almuhtad@ualberta.ca>
2022-07-10 16:07:18 -0600

    hotfix tests to allow ci/cd to run (#31)

--- frontend/src/tests/components/about/Values.test.js
+++ frontend/src/tests/components/about/Values.test.js
@@ -1,11 +1,12 @@
-import { Value } from "../../../components/about";
+import React from 'react'
+import { Value } from '../../../components/about'
 import { render, screen } from '@testing-library/react'
 
 test('renders values', () => {
     render(<Value />)
-    expect(screen.getByRole("value")).toHaveTextContent("1. Mission First")
-    expect(screen.getByRole("value")).toHaveTextContent("2. Perpetual Growth")
-    expect(screen.getByRole("value")).toHaveTextContent("3. Cherish Each Other")
-    expect(screen.getByRole("value")).toHaveTextContent("4. Always Innovate")
-    expect(screen.getByRole("value")).toHaveTextContent("5. Be Humble")
+    expect(screen.getByRole('value')).toHaveTextContent('1. Mission First')
+    expect(screen.getByRole('value')).toHaveTextContent('2. Perpetual Growth')
+    expect(screen.getByRole('value')).toHaveTextContent('3. Cherish Each Other')
+    expect(screen.getByRole('value')).toHaveTextContent('4. Always Innovate')
+    expect(screen.getByRole('value')).toHaveTextContent('5. Be Humble')
 })
--- frontend/src/tests/components/shared/Footer.test.js
+++ frontend/src/tests/components/shared/Footer.test.js
@@ -1,8 +1,11 @@
-import { Footer } from "../../../components/shared";
+import React from 'react'
+import { Footer } from '../../../components/shared'
 import { render, screen } from '@testing-library/react'
 
 test('renders footer', () => {
     render(<Footer />)
-    expect(screen.findAllByLabelText("Blueprint 2022"))
-    expect(screen.getByRole("footer")).toHaveTextContent("Blueprint 2022 General Home General Projects For Students About FAQ For Nonprofits About FAQ Contact Us Email LinkedIn Instagram Github")
+    expect(screen.findAllByLabelText('Blueprint 2022'))
+    expect(screen.getByRole('footer')).toHaveTextContent(
+        'Blueprint 2022 General Home General Projects For Students About FAQ For Nonprofits About FAQ Contact Us Email LinkedIn Instagram Github'
+    )
 })
