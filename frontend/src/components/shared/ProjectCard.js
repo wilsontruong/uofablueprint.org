9780fd52626c968f56b734f3b810d7f77a08d091
GurveerSohal <74379063+GurveerSohal@users.noreply.github.com>
2022-07-13 18:11:35 -0600

    styled project card component (#15)
    doesn't match figma mockup exactly because we want to redesign the card component

--- frontend/src/components/shared/ProjectCard.js
+++ frontend/src/components/shared/ProjectCard.js
@@ -1,7 +1,39 @@
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function ProjectCard() {
-    return <div>-card project component placeholder-</div>
+function ProjectCard({image, header, body}) {
+    return (
+        <Card raised={true} sx={{
+            maxWidth: 345,
+            textAlign: 'center'
+        }}>
+            <CardMedia
+                component='img'
+                height='140'
+                image={image}
+                alt='no image'
+            />
+
+            <CardContent>
+                <Typography gutterBottom={true} variant='h5' component='div' sx={{
+                    fontWeight: 'bold'
+                }}>
+                    {header}
+                </Typography>
+
+                <Typography variant='body1'>
+                    {body}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+ProjectCard.propTypes = {
+    image: PropTypes.string,
+    header: PropTypes.string,
+    body: PropTypes.string
 }
 
 export default ProjectCard
